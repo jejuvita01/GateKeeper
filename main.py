c4ee7c3622e1153f3ca69a0fb1e47ff123b0d197
kim <kyoungbin1996@gmail.com>
2023-12-07 17:05:53 +0900

    fix : main.py to run daemon

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from camera.cam import runCam
+from utils import runJavaScript
+import threading
+import time
 
+
+fileUploadNotTread = threading.Thread(target=runJavaScript.runFileUploadNotification)
+fileUploadNotTread.daemon = True
 
 if __name__=='__main__':
+    fileUploadNotTread.start()
+    time.sleep(1.2)
     runCam()
