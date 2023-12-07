b8d56119b7bfb59632a1ace919471548e36d9ca8
kim <kyoungbin1996@gmail.com>
2023-12-07 17:26:03 +0900

    impl : update runFileupload.js

--- utils/runJavaScript.py
+++ utils/runJavaScript.py
@@ -1,6 +1,9 @@
 import os
 
-
 def runFileUploadNotification():
     os.chdir("./fileuploadnotification/")
     os.system("node ./FileUploadNotification.js")
+
+def runFileUpload():
+    os.chdir("./fileupload/")
+    os.system("node ./FileUpload.js")
